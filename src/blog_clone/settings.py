0f3a5d3a3e21ddc3c44945ce427d76d9b38d9a15
Reynold Figueroa <reynold0801@outlook.com>
2019-01-16 14:00:48 -0400

    Post & Comment
    Models Created

--- src/blog_clone/settings.py
+++ src/blog_clone/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# TEMPLATE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # MY APPS
+    'blog',
 ]
 
 MIDDLEWARE = [
